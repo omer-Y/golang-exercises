122602bb0cd2b5a6484bb75b5a6858a9d556a550
omer-y <om.yelkenci@gamil.com>
2021-12-18 16:46:47 +0300

    - Adding a failure message

--- guess-game/guess.go
+++ guess-game/guess.go
@@ -18,10 +18,10 @@ func main() {
 	target := rand.Intn(100) + 1 // Add 1 to make it an intefer from 1 to 100.
 	fmt.Println("I've chosen a random number between 1 and 100.")
 	fmt.Println("Can zou guess it?")
-	fmt.Println(target)
 
 	reader := bufio.NewReader(os.Stdin) // Create a bufio.Reader, which lets us read keyboard input.
 
+	success := false                            // Declare "success" before the loop, so it's still in scope after the loop exits.
 	for guesses := 0; guesses < 10; guesses++ { // Use the "guesses" variable to track the number of guesses so far.
 		fmt.Println("You have", 10-guesses, "guesses left.") // Subtract the number of guesses from 10 to tell the player how many they have left.
 
@@ -41,8 +41,13 @@ func main() {
 		} else if guess > target {
 			fmt.Println("Oops. Your guess was HIGH.")
 		} else {
+			success = true // If the player guesses correctly, indicate we don't need to print the failure message.
 			fmt.Println("Good job! You guessed it!")
 			break
 		}
+	}
+
+	if !success { // If the player was NOT successful (if "success" is false)
+		fmt.Println("Sorry, you didn't guess my number. It was:", target) // print the failure message.
 	}
 }
