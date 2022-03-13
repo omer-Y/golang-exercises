ba837c80389eaa73717c735cf534b62edf1cb63f
omer-Y <om.yelkenci@gmail.com>
2022-03-13 17:07:58 +0300

    Validate user input.

--- booking-app/main.go
+++ booking-app/main.go
@@ -35,7 +35,11 @@ func main() {
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
 
-		if userTickets <= remainingTickets {
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+		if isValidName && isValidEmail && isValidTicketNumber {
 			remainingTickets = conferenceTickets - userTickets
 			bookings = append(bookings, firstName+" "+lastName)
 
@@ -56,7 +60,15 @@ func main() {
 				break
 			}
 		} else {
-			fmt.Printf("We only %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			if !isValidName {
+				fmt.Println("First name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("Email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Number of tickets your entered is invalid")
+			}
 		}
 	}
 }
