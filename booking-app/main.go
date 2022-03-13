f4b8bcc29d96a1c1f02ba77a5ae283f7b79939ca
omer-Y <om.yelkenci@gmail.com>
2022-03-13 15:47:07 +0300

    Getting user input. What is a pointer? Book ticket logic.

--- booking-app/main.go
+++ booking-app/main.go
@@ -4,22 +4,33 @@ import "fmt"
 
 func main() {
 	conferenceName := "Go Conference" // another method for define variable.
-	const conferenceTickets int = 50  // this variable type cannot be changed of value
+	const conferenceTickets uint = 50 // this variable type cannot be changed of value
 	var remainingTickets uint = 50    // except negative value
-
-	// %T return the variable type
-	fmt.Printf("conferenceTickets is %T, remainingTicets is %T, conferenceName is %T \n", conferenceTickets, remainingTickets, conferenceName)
 
 	// %v return the variable value
 	fmt.Printf("Welcome to %v our conference booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
 
-	userName = "Omer"
-	userTickets = 15
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
 
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = conferenceTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceTickets)
 }
