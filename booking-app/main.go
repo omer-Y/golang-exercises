1f807cc967b0b0cc6a1e601671134c74f1233625
omer-Y <om.yelkenci@gmail.com>
2022-03-13 16:55:26 +0300

    Conditionals(if/else) and Boolean Data Type

--- booking-app/main.go
+++ booking-app/main.go
@@ -35,19 +35,28 @@ func main() {
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
 
-		remainingTickets = conferenceTickets - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+		if userTickets <= remainingTickets {
+			remainingTickets = conferenceTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceTickets)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceTickets)
 
-		firstNames := []string{}
+			firstNames := []string{}
 
-		for _, booking := range bookings { // foreach loop
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			for _, booking := range bookings { // foreach loop
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("These are all our bookings: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			fmt.Printf("We only %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
 		}
-
-		fmt.Printf("These are all our bookings: %v\n", firstNames)
 	}
 }
