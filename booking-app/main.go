ce9edf1e4bab770a8b7be8f70619ebb2fcdd2505
omer-Y <om.yelkenci@gmail.com>
2022-03-13 15:14:23 +0300

    Variables & Constants in Go

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50 // this variable type cannot be changed of value
+	var remainingTicets = 50
+
+	fmt.Println("Welcome to", conferenceName, "our conference booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTicets, "are still available.")
 	fmt.Println("Get your tickets here to attend")
 }
